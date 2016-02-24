dd631108f3966614de1a90a084aa377423a38c47
Nathan Allen <nlallenster@gmail.com>
2016-02-24 13:43:24 -0600

    changed server port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(compress());
 app.use(express.static(__dirname + '/public'))
 
 //Port verification
-var port = 3000;
+
 app.listen(port, function(){
   console.log("Listening on port: ", port);
 });
