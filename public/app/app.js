a8347353695e2dc9eafc2f44ca9facc1b1001d7b
Nathan Allen <nlallenster@gmail.com>
2016-02-25 14:37:38 -0600

    seo site improvements

--- public/app/app.js
+++ public/app/app.js
@@ -1,36 +1,74 @@
-angular.module("macysApp", ["ui.router", "ui.materialize"])
+var app = angular.module("macysApp", ["ngRoute", "ui.materialize"])
 
-.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider) {
 
-  $stateProvider
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+  $locationProvider.hashPrefix('!');
 
-
-    .state('home', {
-    url: '/home',
-    templateUrl: 'app/views/home/homeTmpl.html'
+    $routeProvider.when('/', {
+    templateUrl: 'app/views/home/homeTmpl.html',
+    controller: 'homeController'
   })
 
-  .state('about', {
-    url: '/about',
-    templateUrl: 'app/views/about/aboutTmpl.html'
+  $routeProvider.when('/about', {
+    templateUrl: 'app/views/about/aboutTmpl.html',
+    controller: 'aboutController'
   })
 
-  .state('services', {
-    url: '/services',
-    templateUrl: 'app/views/services/servicesTmpl.html'
+  $routeProvider.when('/services', {
+    templateUrl: 'app/views/services/servicesTmpl.html',
+    controller: 'servicesController'
   })
 
-  .state('contact', {
-    url: '/contact',
-    templateUrl: 'app/views/contact/contactTmpl.html'
+  $routeProvider.when('/contact', {
+    templateUrl: 'app/views/contact/contactTmpl.html',
+    controller: 'contactController'
   })
 
-  // $locationProvider.html5Mode({
-  //   enabled: true,
-  //   requireBase: false
-  // });
-  // $locationProvider.hashPrefix('!');
+  // $routeProvider.when('/sitemap', {
+  //   templateUrl: 'sitemap.xml'
+  // })
 
-  $urlRouterProvider.otherwise('/home');
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
 
-});
+})
+
+.controller('mainController', function ($scope) {
+  $scope.seo = {
+    pageTitle: '',
+    pageDescription: ''
+  };
+})
+
+.controller('homeController', function ($scope) {
+  $scope.$parent.seo = {
+    pageTitle: 'Your one stop glass shop',
+    pageDescription: 'Your one stop shop for shower doors, custom mirrors, commercial glass, auto glass, flat glass, safety glass.'
+  };
+})
+
+.controller('servicesController', function ($scope) {
+  $scope.$parent.seo = {
+    pageTitle: 'Services',
+    pageDescription: 'Check out our comprehensive list of services that fulfill all of your glass needs.'
+  };
+})
+
+.controller('aboutController', function ($scope) {
+  $scope.$parent.seo = {
+    pageTitle: 'About',
+    pageDescription: 'We have been in business for over 30 years, and we have the experience to serve you.'
+  };
+})
+
+.controller('contactController', function ($scope) {
+  $scope.$parent.seo = {
+    pageTitle: 'Contact',
+    pageDescription: 'We look forward to serving you.'
+  };
+})
--- server.js
+++ server.js
@@ -7,15 +7,15 @@ var express = require('express');
 //Express Connection
 var port = process.env.PORT;
 var app = express();
-
-
 app.use(compress());
-// app.use(require('prerender-node').set('prerenderToken', 'PCayKoAiRyC2eJaFeVNA'));
-app.use(express.static(__dirname + '/public'))
 
-// app.get('*', function(req, res){
-//   res.sendfile('./public/index.html');
-// });
+
+app.use(require('prerender-node').set('prerenderToken', 'PCayKoAiRyC2eJaFeVNA'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res){
+  res.sendfile('./public/index.html');
+});
 
 //Port verification
 app.listen(port, function(){
